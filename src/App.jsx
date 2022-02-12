bc64a0663732887586aac36fff56d6cd7828bd48
dennyrismanto <rismanto.denny@gmail.com>
2022-02-12 07:20:26 +0700

    local image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-
+import side_atas_image from "./public/photos/side_atas.png";
+import side_bawah_image from "./public/photos/side_bawah.png";
+import frame_atas_image from "./public/photos/frame_atas.png";
+import frame_bawah_image from "./public/photos/frame_bawah.png";
 import "./App.css";
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
       <div class="flex justify-start items-start">
         <img
           class="w-28 h-28 md:w-56 md:h-56 md:rounded-none rounded-none"
-          src="/src/public/photos/side_atas.png"
+          src={side_atas_image}
           alt="side1"
         ></img>
       </div>
@@ -19,7 +21,7 @@ function App() {
         <p class="text-yellow-600 text-lg md:text-xl">Invitation</p>
         <img
           class="mt-6 mb-6 h-20 w-52 md:w-1/4 md:h-3/4"
-          src="/src/public/photos//frame_atas.png"
+          src={frame_atas_image}
           alt="frame"
         />
         <p class="text-yellow-600 m-2 md:p-0 text-xl md:text-4xl">
@@ -27,7 +29,7 @@ function App() {
         </p>
         <img
           class="mt-6 mb-6 h-20 w-52 md:w-1/4 md:h-3/4"
-          src="/src/public/photos//frame_bawah.png"
+          src={frame_bawah_image}
           alt="frame"
         />
         <p class="text-yellow-600 text-lg pt-2 md:text-xl md:pt-6">
@@ -43,7 +45,7 @@ function App() {
       <div class="flex justify-end items-end">
         <img
           class="w-32 h-32 -mt-9 md:w-56 md:h-56 md:-mt-60 md:rounded-none rounded-none"
-          src="/src/public/photos/side_bawah.png"
+          src={side_bawah_image}
           alt="side1"
         />
       </div>
